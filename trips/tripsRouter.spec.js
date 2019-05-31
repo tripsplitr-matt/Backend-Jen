e16f210b4b24bcb217503f5d6ee64bb20b011407
ohheyimjen <jendority@gmail.com>
2019-05-31 13:17:39 -0700

    commented out testing

--- trips/tripsRouter.spec.js
+++ trips/tripsRouter.spec.js
@@ -1,18 +1,16 @@
-require('dotenv').config()
+// require("dotenv").config();
 
-const db = require('../data/dbConfig');
+// const db = require("../data/dbConfig");
 
+// describe("trips model", () => {
+//   beforeEach(async () => {
+//     await db("trips").truncate();
+//   });
 
-describe('trips model', () => {
-
-  beforeEach(async () => {
-    await db('trips').truncate();  
-  })
-
-  describe('get all()', () => {
-    it('should always return an array, even if no trips are stored', async () => {
-      const trips = await db('trips');
-      expect(trips)
-    })
-  })
-})
+//   describe("get all()", () => {
+//     it("should always return an array, even if no trips are stored", async () => {
+//       const trips = await db("trips");
+//       expect(trips);
+//     });
+//   });
+// });
--- users/usersRouter.spec.js
+++ users/usersRouter.spec.js
@@ -1,18 +1,17 @@
-require('dotenv').config()
+// require("dotenv").config();
 
-const db = require('../data/dbConfig');
-const Users = require('./usersModel');
+// const db = require("../data/dbConfig");
+// const Users = require("./usersModel");
 
-describe('users model', () => {
+// describe("users model", () => {
+//   beforeEach(async () => {
+//     await db("users").truncate();
+//   });
 
-  beforeEach(async () => {
-    await db('users').truncate();  
-  })
-
-  describe('get all()', () => {
-    it('should always return an array, even if no games are stored', async () => {
-      const users = await db('users');
-      expect(users)
-    })
-  })
-})
+//   describe("get all()", () => {
+//     it("should always return an array, even if no games are stored", async () => {
+//       const users = await db("users");
+//       expect(users);
+//     });
+//   });
+// });
