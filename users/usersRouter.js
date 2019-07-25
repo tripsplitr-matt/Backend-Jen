196a216f2176d3b01a25ba9c06145693d4c466ae
Leslie Thompson <lcrthompson@gmail.com>
2019-07-25 15:56:54 -0700

    user update

--- users/usersRouter.js
+++ users/usersRouter.js
@@ -1,78 +1,86 @@
-const router = require('express').Router();
-const Users = require ('./usersModel');
+const router = require("express").Router();
+const Users = require("./usersModel");
 // const restricted = require('../auth/restricted-middleware');
 
 // retrieve all users
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     const users = await Users.find();
     res.status(200).json(users);
   } catch (error) {
-    res.status(500).json({ error: 'Uh-oh! There was an error retrieving your list of users'})
+    res
+      .status(500)
+      .json({
+        error: "Uh-oh! There was an error retrieving your list of users"
+      });
   }
 });
 
 // retrieve specific users by id #
-router.get('/:id', async (req, res) => {
+router.get("/:id", async (req, res) => {
   try {
     const user = await Users.findById(req.params.id);
 
     if (user) {
       res.status(200).json(user);
     } else {
-      res.status(404).json({ message: 'User not found' });
+      res.status(404).json({ message: "User not found" });
     }
   } catch (error) {
     // log error to database
     console.log(error);
     res.status(500).json({
-      message: 'Error retrieving the person you\'re looking for',
+      message: "Error retrieving the person you're looking for"
     });
   }
 });
 
 // remove a user
-router.delete('/:id', async (req, res) => {
+router.delete("/:id", async (req, res) => {
   try {
     const count = await Users.remove(req.params.id);
     if (count > 0) {
-      res.status(200).json({ message: 'The user has been removed from the trip...au revoir!' })
+      res
+        .status(200)
+        .json({
+          message: "The user has been removed from the trip...au revoir!"
+        });
     } else {
-      res.status(404).json({ message: 'Uh-oh! Action could not found.' })
+      res.status(404).json({ message: "Uh-oh! Action could not found." });
     }
   } catch (error) {
     res.status(500).json({
-      message: 'Unable to remove user. Can\'t get rid of them that easy!',
+      message: "Unable to remove user. Can't get rid of them that easy!"
     });
   }
 });
 
 // add a user
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   try {
     const user = await Users.add(req.body);
     res.status(200).json(user);
   } catch (error) {
     res.status(500).json({
-      message: 'Bummer. Error adding the user',
+      message: "Bummer. Error adding the user"
     });
   }
-})
+});
 
 // update a user
-router.put('/:id', async (req, res) => {
+router.put("/:id", async (req, res) => {
+  const id = req.params.id;
+  const changes = req.body;
   try {
-    const user = await Users.update(req.params.id, req.body);
+    const user = await Users.update(id, changes);
     if (user) {
       res.status(200).json(user);
     } else {
-      res.status(404).json({ message: 'Uh-oh! User could not be found' })
+      res.status(404).json({ message: "Uh-oh! User could not be found" });
     }
   } catch (error) {
-    res.status(500).json({ message: 'Bummer. Error updating the user' })
+    res.status(500).json({ message: "Bummer. Error updating the user" });
   }
-})
-
-
+});
 
 module.exports = router;
