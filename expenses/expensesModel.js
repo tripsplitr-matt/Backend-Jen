2c86f28ebd891af0d19f2f2f38d56f6dd09a9b6b
Leslie Thompson <lcrthompson@gmail.com>
2019-07-25 16:31:17 -0700

    updates to what get expenses returns

--- expenses/expensesModel.js
+++ expenses/expensesModel.js
@@ -1,28 +1,36 @@
-const db = require('../data/dbConfig');
+const db = require("../data/dbConfig");
 
 module.exports = {
   add,
   find,
   findBy,
-  findById,
+  findById
 };
 
 function find() {
-  return db('expenses').select('expense_name', 'total_expense_price', 'primary_paid');
+  return db("expenses").select(
+    "id",
+    "expense_name",
+    "total_expense_price",
+    "primary_paid",
+    "trip_id",
+    "created_at",
+    "updated_at"
+  );
 }
 
 function findBy(filter) {
-  return db('expenses').where(filter);
+  return db("expenses").where(filter);
 }
 
 async function add(trip) {
-  const [id] = await db('expenses').insert(trip);
+  const [id] = await db("expenses").insert(trip);
 
   return findById(id);
 }
 
 function findById(id) {
-  return db('expenses')
+  return db("expenses")
     .where({ id })
     .first();
 }
